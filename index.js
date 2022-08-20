ba1015c419391df61ff586f9bff0897da706b287
SparkyFnay <z1362110538@gmail.com>
2022-08-20 12:27:07 -0400

    myb

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-import mongoose from 'mongoose';
-import express from 'express';
-import { familyMembers } from './routes/family-members.js';
+import mongoose from "mongoose";
+import express from "express";
+import cors from "cors";
+import { familyMembers } from "./routes/family-members.js";
 const app = express();
 
-
-mongoose.connect('mongodb://localhost/householdexpenses')
-.then(() => console.log('connected to MongoDB'))
-.catch(err => console.error('could not connect to MongoDB: ', err));
+mongoose
+  .connect("mongodb://localhost/householdexpenses")
+  .then(() => console.log("connected to MongoDB"))
+  .catch((err) => console.error("could not connect to MongoDB: ", err));
 
 // put routes here -> app.use('/somerout', name)
+app.use(cors());
 app.use(express.json());
-app.use('/api/family-members', familyMembers);
+app.use("/api/family-members", familyMembers);
 
-const port  = 2000;
+const port = 2000;
 app.listen(port, () => console.log(`listening on port ${port}`));
 
-app.get('/', (req, res) => {
-    res.send('Househould Expenses');
+app.get("/", (req, res) => {
+  res.send("Househould Expenses");
 });
--- routes/family-members.js
+++ routes/family-members.js
@@ -1,60 +1,74 @@
-import express from 'express';
-import { FamilyMember } from '../models/family-member.js';
+import express from "express";
+import { FamilyMember } from "../models/family-member.js";
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-    const familyMembers = await FamilyMember.find().sort('firstName');
-    res.send(familyMembers);
+router.get("/", async (req, res) => {
+  const familyMembers = await FamilyMember.find().sort("firstName");
+  res.send(familyMembers);
 });
 
-router.get('/:id', async(req, res) => {
-    try{
-        const familyMember = await FamilyMember.findById(req.params.id);
-        res.send(familyMember);
-    } catch(ex) {
-        return res.status(404).send('The family member with the given ID was not found');
-    }
-});
-
-router.post('/', async(req, res) => {
-    let familyMember = new FamilyMember({
-        firstName: req.body.firstName,
-        lastName: req.body.lastName
-    });
-
-    try {
-        familyMember = await familyMember.save();
-    } catch(ex) {
-        for (field in ex.errors) {
-            console.log(ex.errors[field].message);
-        }
-    }
+router.get("/:id", async (req, res) => {
+  try {
+    const familyMember = await FamilyMember.findById(req.params.id);
     res.send(familyMember);
+  } catch (ex) {
+    return res
+      .status(404)
+      .send("The family member with the given ID was not found");
+  }
 });
 
-router.put('/:id', async (req, res) => {
-    try {
-        const familyMember = await FamilyMember.findByIdAndUpdate(req.params.id, {
-            firstName: req.body.firstName,
-            lastName: req.body.lastName,
-        }, {
-            new: true
-        });
-        res.send(familyMember);
-    } catch(ex) {
-        return res.status(404).send('The family member with the given ID was not found');
+router.post("/", async (req, res) => {
+  console.log("body", JSON.stringify(req.body));
+  let familyMember = new FamilyMember({
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+  });
+
+  try {
+    console.log(familyMember);
+    familyMember = await familyMember.save();
+  } catch (ex) {
+    console.log(ex);
+    if (typeof ex.errors === "array") {
+      for (field in ex.errors) {
+        console.log(ex.errors[field].message);
+      }
     }
+  }
+  res.send(familyMember);
 });
 
-router.delete('/:id', async (req, res)  => {
-    try {
-        const familyMember  = await FamilyMember.findByIdAndRemove(req.params.id)
-        res.send(familyMember);
-    }
-    catch(ex){
-        return res.status(404).send('The family member with the given ID was not found');
-    }
+router.put("/:id", async (req, res) => {
+  try {
+    const familyMember = await FamilyMember.findByIdAndUpdate(
+      req.params.id,
+      {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+      },
+      {
+        new: true,
+      }
+    );
+    res.send(familyMember);
+  } catch (ex) {
+    return res
+      .status(404)
+      .send("The family member with the given ID was not found");
+  }
 });
 
-export {router as familyMembers};
+router.delete("/:id", async (req, res) => {
+  try {
+    const familyMember = await FamilyMember.findByIdAndRemove(req.params.id);
+    res.send(familyMember);
+  } catch (ex) {
+    return res
+      .status(404)
+      .send("The family member with the given ID was not found");
+  }
+});
+
+export { router as familyMembers };
