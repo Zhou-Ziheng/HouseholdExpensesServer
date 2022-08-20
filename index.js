d0ee0e7da26ce436db5b3fc0c3a79ce04de29406
Rose <rose.goldfarb26@gmail.com>
2022-08-19 21:33:58 -0400

    add route for family members

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
 import express from 'express';
+import { familyMembers } from './routes/family-members.js';
 const app = express();
 
 
@@ -9,8 +10,9 @@ mongoose.connect('mongodb://localhost/householdexpenses')
 
 // put routes here -> app.use('/somerout', name)
 app.use(express.json());
+app.use('/api/family-members', familyMembers);
 
-const port  = 3000;
+const port  = 2000;
 app.listen(port, () => console.log(`listening on port ${port}`));
 
 app.get('/', (req, res) => {
